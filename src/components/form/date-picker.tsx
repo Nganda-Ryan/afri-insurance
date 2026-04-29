4ad9fab239d35dc37b28b66d622bf4e43b9f3310
STEVE LOIC NGANDA ONANA <steveloicnganda@gmail.com>
2026-04-29 07:58:23 +0100

    .

--- src/components/form/date-picker.tsx
+++ src/components/form/date-picker.tsx
@@ -64,7 +64,6 @@ export default function DatePicker({
 
     const flatPickr = flatpickr(inputElement, {
       mode: mode || 'single',
-      disableMobile: true,
       static: true,
       monthSelectorType: 'static',
       dateFormat: 'Y-m-d',
@@ -110,11 +109,11 @@ export default function DatePicker({
           onBlur={onBlur}
           readOnly
           placeholder={placeholder}
-          className={`h-11 w-full appearance-none rounded-lg border bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 ${inputStateClasses} ${className || ''}`}
+          className={`hidden md:block h-11 w-full appearance-none rounded-lg border bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 ${inputStateClasses} ${className || ''}`}
           autoComplete="off"
         />
 
-        <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
+        <span className="hidden md:block absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
           <CalenderIcon className="size-6" />
         </span>
       </div>
